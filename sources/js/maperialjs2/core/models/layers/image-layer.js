01e28985778cfcf08f5088b0e67453d6e28fc6a3
Chris <chris.dugne@uralys.com>
2014-01-05 21:55:58 +0100

    image composition

--- sources/js/maperialjs2/core/models/layers/image-layer.js
+++ sources/js/maperialjs2/core/models/layers/image-layer.js
@@ -1,10 +1,11 @@
 //-----------------------------------------------------------------------------------//
 
-function ImageLayer (sourceId) {
+function ImageLayer (sourceId, composition) {
    
    this.id              = Utils.generateUID();
    this.type            = Layer.Images;
    this.sourceId        = sourceId;
+   this.composition     = composition;
    
 }
 
