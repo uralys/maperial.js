c4269bbb45faa6cd912ae8176a649a1dc9aea32e
chris <chris.dugne@gmail.com>
2014-11-17 15:17:22 +0100

    started anchor

---
+++ sources/js/maperialjs/tools/simple-zoom.js
@@ -0,0 +1,42 @@
+//------------------------------------------------------------------------------
+
+var CoordinateSystem = require('../libs/coordinate-system.js');
+
+//------------------------------------------------------------------------------
+
+function SimpleZoom(options) {
+
+    var container = null;
+    var views = options.views;
+    var zoomIn = document.createElement('div');
+    var zoomOut = document.createElement('div');
+
+    if (options.container) {
+        container = document.getElementById(options.container);
+    } else {
+        container = document.createElement('div');
+        views[0].container.appendChild(container);
+    }
+
+    zoomIn.className = "maperial-simple-zoom-in";
+    zoomOut.className = "maperial-simple-zoom-out";
+
+    container.appendChild(zoomIn);
+    container.appendChild(zoomOut);
+
+    zoomIn.addEventListener("click", function () {
+        views.forEach(function (view) {
+            view.trigger("zoom-in");
+        });
+    });
+
+    zoomOut.addEventListener("click", function () {
+        views.forEach(function (view) {
+            view.trigger("zoom-out");
+        });
+    });
+}
+
+//------------------------------------------------------------------------------
+
+module.exports = SimpleZoom;
