09d873fdc4d2f7d5d716d3499e3c8023a20c3d7c
chris <chris.dugne@gmail.com>
2014-11-20 12:17:00 +0100

    quickfix for dynamical rendering..

--- sources/js/maperialjs/core/rendering/map-renderer.js
+++ sources/js/maperialjs/core/rendering/map-renderer.js
@@ -190,16 +190,22 @@ MapRenderer.prototype.drawScene = function () {
 
     //---------------------------------------------------------------
 
-    for (var rendererId in this.dynamicalRenderers) {
-        var renderer = this.dynamicalRenderers[rendererId];
-        renderer.synchronize(
-            this.mapView.context.zoom,
-            tileC.x,
-            tileC.y - nbTileY,
-            nbTileX + 1,
-            nbTileY + 1
-        );
-    }
+    // @FIXME
+    // bug : once in a while data is not displayed : only with a image layer
+    // quickfix -> delaying the dynamicalRenderers sync enhance this behaviour..
+    setTimeout(function(){
+        for (var rendererId in this.dynamicalRenderers) {
+            var renderer = this.dynamicalRenderers[rendererId];
+            renderer.synchronize(
+                this.mapView.context.zoom,
+                tileC.x,
+                tileC.y - nbTileY,
+                nbTileX + 1,
+                nbTileY + 1
+            );
+        }
+
+    }.bind(this), 150);
 
     //---------------------------------------------------------------
 
